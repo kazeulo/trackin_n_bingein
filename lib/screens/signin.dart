4c4c92fd67209ed06589c57b132c72e95722b1e6
Gliezel Ann Pajarilla <gtpajarilla@up.edu.ph>
2024-05-17 23:19:14 +0800

    changes made to the main

---
+++ lib/screens/signin.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+
+  const SignIn({super.key});
+
+  @override
+  SignInState createState() => SignInState();
+}
+
+class SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
