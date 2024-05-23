257736f574026877795c43fbb4359ab923b3041f
Ma. Christina Kane Vito <91617687+Kanedykane@users.noreply.github.com>
2024-05-24 00:15:33 +0800

    initial look

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,7 +1,53 @@
 import 'package:flutter/material.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   const Homepage({Key? key}) : super(key: key);
+
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _tabs = [
+    Container(
+      color: Colors.white,
+      child: Center(
+        child: Text(
+          'Home',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+    ),
+    Container(
+      color: Colors.white,
+      child: Center(
+        child: Text(
+          'Statistics',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+    ),
+    Container(
+      color: Colors.white,
+      child: Center(
+        child: Text(
+          'List',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+    ),
+    Container(
+      color: Colors.white,
+      child: Center(
+        child: Text(
+          'Profile',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -9,14 +55,32 @@ class Homepage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Homepage'),
       ),
-      body: Container(
-        color: Colors.white,
-        child: Center(
-          child: Text(
-            'Welcome to the Homepage!',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      body: _tabs[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, color: Colors.black),
+            label: '',
           ),
-        ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.analytics, color: Colors.black),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list, color: Colors.black),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person, color: Colors.black),
+            label: '',
+          ),
+        ],
       ),
     );
   }
