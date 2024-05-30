3c8259a3ace74f0b8a9050b6a57d2da06b4742c2
Ma. Christina Kane Vito <91617687+Kanedykane@users.noreply.github.com>
2024-05-30 14:11:34 +0800

    Update profile.dart

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:trackin_n_bingein/screens/editprofile.dart';
 import 'package:trackin_n_bingein/styling/styling.dart';
+import 'package:trackin_n_bingein/screens/signin.dart';
 
 class Profile extends StatelessWidget {
   @override
@@ -25,14 +25,7 @@ class Profile extends StatelessWidget {
             ),
             
           ),
-          // Center(
-          //       child: Image.asset(
-          //         "lib/assets/logofin.png",
-          //         width: 80,
-          //         height: 90,
-          //       ),
-          //     ),
-              Text(
+          Text(
             "Customize your page",
             style: TextStyle(
               fontSize: 18,
@@ -44,7 +37,7 @@ class Profile extends StatelessWidget {
 
           SizedBox(height: 10), 
           Padding(
-            padding: EdgeInsets.only(top:90),
+            padding: EdgeInsets.only(top: 90),
             child: Container(
               child: Expanded(
                 child: Center(
@@ -54,12 +47,11 @@ class Profile extends StatelessWidget {
                       buildCardWithIcon(
                         title: 'Edit Profile',
                         icon: Icons.person,
-                        // implement b-end
                         onTap: () {
                           Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const EditProfile()),
-                    );
+                            context,
+                            MaterialPageRoute(builder: (context) => const EditProfile()),
+                          );
                         },
                       ),
                       SizedBox(height: 10),
@@ -67,7 +59,7 @@ class Profile extends StatelessWidget {
                         title: 'Log Out',
                         icon: Icons.logout,
                         onTap: () {
-                          // implement b-end
+                          showLogoutConfirmationDialog(context);
                         },
                       ),
                       SizedBox(height: 10),
@@ -89,17 +81,46 @@ class Profile extends StatelessWidget {
     );
   }
 
-  Widget buildCardWithIcon(
-    
-      {required String title,
-      required IconData icon,
-      required VoidCallback onTap,
-      bool isDarker = false,
-      Color? cardColor,
-      }) {
-        // for color wanted to implement delete to be a shade darker
-        cardColor??=isDarker ? Styling.textColor1 : Color.fromARGB(255, 197, 219, 221);
-    
+  void showLogoutConfirmationDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Confirm Logout"),
+          content: Text("Are you sure you want to log out?"),
+          actions: [
+            TextButton(
+              child: Text("No"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text("Yes"),
+              onPressed: () {
+                Navigator.of(context).pop();
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => Signin()),
+                );
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget buildCardWithIcon({
+    required String title,
+    required IconData icon,
+    required VoidCallback onTap,
+    bool isDarker = false,
+    Color? cardColor,
+  }) {
+    // for color wanted to implement delete to be a shade darker
+    cardColor ??= isDarker ? Styling.textColor1 : Color.fromARGB(255, 197, 219, 221);
+
     return Card(
       color: cardColor,
       elevation: 4,
@@ -112,17 +133,17 @@ class Profile extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Expanded(
-                child: Text(title,
-                textAlign: TextAlign.start,
-                style: TextStyle(fontSize: 16, 
-                fontWeight: FontWeight.normal,
-                color: Styling.textColor3,
+                child: Text(
+                  title,
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.normal,
+                    color: Styling.textColor3,
+                  ),
                 ),
-                ),
-                   
               ),
               Icon(icon),  
-               
             ],
           ),
           onTap: onTap,
